343fbacd43be35dc046f626fbd7a4a713011610c
FizzBuzz <krishnanikunjpatel09@gmail.com>
2024-02-28 20:12:50 -0500

    Created logic for test case for number which is divisible by 5 which should return Buzz Result: Test passed

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,9 @@ public class App
         if (n % 3 == 0) {
             return "Fizz";
         }
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(n);
     }
 }
